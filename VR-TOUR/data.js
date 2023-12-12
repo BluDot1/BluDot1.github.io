052722038e07ae19a8e55df7a7eb090ede7fdf8d
Yon48 <151472091+Yon48@users.noreply.github.com>
2023-12-12 21:29:44 +0300

    SHEGEN

--- VR-TOUR/data.js
+++ VR-TOUR/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-chairman-side",
-      "name": "CHAIRMAN SIDE",
+      "id": "0-view-entrance-side",
+      "name": "VIEW ENTRANCE SIDE",
       "levels": [
         {
           "tileSize": 256,
@@ -30,35 +30,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.0380981487442504,
-          "pitch": 0.10644876484531096,
+          "yaw": -0.3526457223807711,
+          "pitch": -0.05619816536572486,
           "rotation": 0,
-          "target": "2-right-side"
+          "target": "2-bar-view"
         },
         {
-          "yaw": -0.68491294114075,
-          "pitch": 0.04247631391372586,
+          "yaw": 0.0878650238020775,
+          "pitch": 0.11107118039591057,
           "rotation": 0,
-          "target": "1-left-side"
+          "target": "3-bar-view-2"
         },
         {
-          "yaw": -3.1033752357022326,
-          "pitch": 0.1819604606412799,
+          "yaw": 0.8451489046923921,
+          "pitch": 0.024870091658758753,
           "rotation": 0,
-          "target": "0-chairman-side"
+          "target": "1-vip"
         },
         {
-          "yaw": -0.005451099167812501,
-          "pitch": 0.09079491607811718,
+          "yaw": 0.45529576066381416,
+          "pitch": -0.014380525419648293,
           "rotation": 0,
-          "target": "3-tv-side"
+          "target": "4-back-view"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-left-side",
-      "name": "LEFT SIDE",
+      "id": "1-vip",
+      "name": "VIP",
       "levels": [
         {
           "tileSize": 256,
@@ -86,29 +86,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.22748476395483408,
-          "pitch": 0.048604866489071696,
+          "yaw": -2.6738566782463753,
+          "pitch": 0.09510893588483782,
           "rotation": 0,
-          "target": "0-chairman-side"
+          "target": "4-back-view"
         },
         {
-          "yaw": -0.5440101246759248,
-          "pitch": 0.08523576198052751,
+          "yaw": 2.09434708781645,
+          "pitch": 0.11418010011835378,
           "rotation": 0,
-          "target": "2-right-side"
+          "target": "3-bar-view-2"
         },
         {
-          "yaw": -1.1581773779565339,
-          "pitch": 0.1906007028364627,
+          "yaw": 2.4310996878429405,
+          "pitch": 0.03228332984794413,
           "rotation": 0,
-          "target": "3-tv-side"
+          "target": "2-bar-view"
+        },
+        {
+          "yaw": 1.7926032416093998,
+          "pitch": 0.08324454032568696,
+          "rotation": 0,
+          "target": "0-view-entrance-side"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-right-side",
-      "name": "RIGHT SIDE",
+      "id": "2-bar-view",
+      "name": "BAR VIEW",
       "levels": [
         {
           "tileSize": 256,
@@ -136,23 +142,35 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.3195874114036137,
-          "pitch": 0.06039316031553277,
+          "yaw": 1.3405595594978355,
+          "pitch": 0.11644513343571106,
           "rotation": 0,
-          "target": "0-chairman-side"
+          "target": "4-back-view"
         },
         {
-          "yaw": 0.7366530199777443,
-          "pitch": 0.14905872924588692,
+          "yaw": 2.45740530764048,
+          "pitch": 0.15268884795574422,
           "rotation": 0,
-          "target": "1-left-side"
+          "target": "1-vip"
+        },
+        {
+          "yaw": -2.308036466213931,
+          "pitch": 0.554401440640989,
+          "rotation": 0,
+          "target": "3-bar-view-2"
+        },
+        {
+          "yaw": -2.167676366586605,
+          "pitch": 0.19831885077407208,
+          "rotation": 0,
+          "target": "0-view-entrance-side"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-tv-side",
-      "name": "TV SIDE",
+      "id": "3-bar-view-2",
+      "name": "BAR VIEW 2",
       "levels": [
         {
           "tileSize": 256,
@@ -180,28 +198,90 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.005309752113710431,
-          "pitch": 0.047751287722787694,
+          "yaw": -0.5750006615164622,
+          "pitch": -0.024714716365023648,
           "rotation": 0,
-          "target": "0-chairman-side"
+          "target": "2-bar-view"
         },
         {
-          "yaw": -1.181522948750498,
-          "pitch": 0.1531397360003126,
+          "yaw": 0.06312965696885087,
+          "pitch": 0.0478147633536512,
           "rotation": 0,
-          "target": "2-right-side"
+          "target": "4-back-view"
         },
         {
-          "yaw": 1.7472124458866558,
-          "pitch": 0.15032070454985202,
+          "yaw": 0.8724911192897391,
+          "pitch": 0.07418065566634269,
           "rotation": 0,
-          "target": "1-left-side"
+          "target": "1-vip"
+        },
+        {
+          "yaw": 2.9401531938528684,
+          "pitch": 0.2465651123872714,
+          "rotation": 0,
+          "target": "0-view-entrance-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-back-view",
+      "name": "BACK VIEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.583254271681767,
+          "pitch": -0.007375885548940531,
+          "rotation": 0,
+          "target": "2-bar-view"
+        },
+        {
+          "yaw": 0.4086384759700543,
+          "pitch": 0.05131453259581065,
+          "rotation": 0,
+          "target": "3-bar-view-2"
+        },
+        {
+          "yaw": 0.26738703778965167,
+          "pitch": 0.014204002529890403,
+          "rotation": 0,
+          "target": "0-view-entrance-side"
+        },
+        {
+          "yaw": -0.2755891387016227,
+          "pitch": 0.05097736210296944,
+          "rotation": 0,
+          "target": "1-vip"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "BLACK LION HOSPITAL MEETING ROOM",
+  "name": "SHEGEN BAR",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
